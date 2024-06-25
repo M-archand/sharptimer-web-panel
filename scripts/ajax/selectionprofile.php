45b606cdcb787110b4d3433e16e9c0b206911767
Marchand <13469995+M-archand@users.noreply.github.com>
2024-06-25 16:11:15 -0400

    Add `TimerTicks` to select statements

--- scripts/ajax/selectionprofile.php
+++ scripts/ajax/selectionprofile.php
@@ -10,10 +10,10 @@
     #$view = "CREATE VIEW test AS SELECT `SteamID`, `PlayerName`, `FormattedTime`, `MapName`, RANK() OVER(ORDER BY `TimerTicks` ASC) AS 'Ranking' FROM PlayerRecords WHERE MapName LIKE 'surf_ace' LIMIT 100; ";
     if($id === "%"){
         //$sql = "SELECT `SteamID`, `PlayerName`, `FormattedTime`, `MapName`, `TimesFinished` FROM PlayerRecords WHERE MapName LIKE '{$id}' AND SteamID = '{$sid}'  ORDER BY `TimerTicks` ASC LIMIT $limit";
-        $sql = "SELECT `SteamID`, `PlayerName`, `FormattedTime`, `MapName`, `TimesFinished`, RANK() OVER(ORDER BY `TimerTicks` ASC) AS 'Ranking' FROM PlayerRecords WHERE MapName LIKE '{$id}' ORDER BY `TimerTicks` ASC";
+        $sql = "SELECT `SteamID`, `PlayerName`, `FormattedTime`, `MapName`, `TimesFinished`, `TimerTicks`, RANK() OVER(ORDER BY `TimerTicks` ASC) AS 'Ranking' FROM PlayerRecords WHERE MapName LIKE '{$id}' ORDER BY `TimerTicks` ASC";
     }
     else{
-        $sql = "SELECT `SteamID`, `PlayerName`, `FormattedTime`, `MapName`, `TimesFinished`, RANK() OVER(ORDER BY `TimerTicks` ASC) AS 'Ranking' FROM PlayerRecords WHERE MapName LIKE '{$id}'";
+        $sql = "SELECT `SteamID`, `PlayerName`, `FormattedTime`, `MapName`, `TimesFinished`, `TimerTicks`, RANK() OVER(ORDER BY `TimerTicks` ASC) AS 'Ranking' FROM PlayerRecords WHERE MapName LIKE '{$id}'";
     }
     ShowRowsProfile($sql, $sid);
 
